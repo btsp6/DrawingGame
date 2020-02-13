766824fd8d8ca938ee2d2683af5c7f0f9e267214
Daniel Liu <hi.daniel.liu@gmail.com>
2020-02-13 01:10:41 -0500

    added gamecontroller and pictures script shells

---
+++ Assets/Scripts/Pictures.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+/*
+ * Storage and access for all the different pictures that users can try to draw
+ */
+public static class Pictures
+{
+
+}
