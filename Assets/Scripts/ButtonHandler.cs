d538ccc91710191fa499528ea1d3d070cfff4aef
Daniel Liu <hi.daniel.liu@gmail.com>
2020-02-15 11:35:52 -0500

    added main menu

---
+++ Assets/Scripts/ButtonHandler.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ButtonHandler : MonoBehaviour
+{
+    public GameObject HelpPanel;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        HelpPanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("GameScene"); 
+    }
+
+    public void OpenHelpPanel()
+    {
+        HelpPanel.SetActive(true);
+    }
+
+    public void CloseHelpPanel()
+    {
+        HelpPanel.SetActive(false);
+    }
+}
