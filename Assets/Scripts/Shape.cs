41a11182066fdafa7a19942f2e1fb3adf08d0fa5
ed1d1a8d <tong.t.wang@gmail.com>
2020-02-16 13:38:04 -0500

    Add basic shapes and manipulation (#1)
    * Add Square and Triangle
    
    * Add Shape.cs

---
+++ Assets/Scripts/Shape.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shape : MonoBehaviour
+{
+    public float moveSpeed = 10f;
+    public float rotateSpeed = 100f;
+    public float scaleSpeed = 0.5f;
+
+    public bool isPlaced = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Shape is alive");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isPlaced)
+        {
+            if (Input.GetButton("PlaceShape"))
+            {
+                isPlaced = true;
+                return;
+            }
+
+            // TODO: Figure out if we want GetAxis or GetAxisRaw
+            Vector3 moveDir = new Vector3(Input.GetAxis("MoveX"), Input.GetAxis("MoveY"), 0);
+            this.transform.position += Time.deltaTime * moveSpeed * moveDir;
+
+            this.transform.Rotate(0, 0, Time.deltaTime * rotateSpeed * Input.GetAxis("Rotate"));
+
+            Vector3 scaleDir = new Vector3(Input.GetAxis("ScaleX"), Input.GetAxis("ScaleY"), 0);
+            this.transform.localScale += Time.deltaTime * moveSpeed * scaleDir;
+        }
+    }
+}
