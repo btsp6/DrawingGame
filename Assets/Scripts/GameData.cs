7fbb2682c06850f507ec1d5ca6cc0819e70e2936
Daniel Liu <hi.daniel.liu@gmail.com>
2020-02-18 17:37:43 -0500

    added scoring functionality

--- Assets/Scripts/GameData.cs
+++ Assets/Scripts/GameData.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public static class GameData
 {
     private static GameState gameState; // TODO: Remove getters and setters?
+    private static int[] scores = new int[3];
     public static GameObject activeShape;
     public static List<GameObject> placedShapes = new List<GameObject>();
 
@@ -16,5 +17,28 @@ public static class GameData
     public static void setGameState(GameState gameState)
     {
         GameData.gameState = gameState;
+    }
+
+    public static void increaseScore(int index)
+    {
+        scores[index]++;
+    }
+
+    public static void resetScore()
+    {
+        for (int i = 0; i < scores.Length; i++)
+        {
+            scores[i] = 0;
+        }
+    }
+
+    public static int getScore(int index)
+    {
+        return scores[index];
+    }
+
+    public static int[] getScores()
+    {
+        return scores;
     }
 }
