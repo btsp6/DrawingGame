d2a6607d0e0d2d8ebb3d95af1243fdb4dae3ac8c
ed1d1a8d <tong.t.wang@gmail.com>
2020-02-18 16:42:29 -0500

    Scaling can't go negative

--- Assets/Scripts/Shape.cs
+++ Assets/Scripts/Shape.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,6 +7,7 @@ public class Shape : MonoBehaviour
     public float moveSpeed = 10f;
     public float rotateSpeed = 100f;
     public float scaleSpeed = 0.5f;
+    private float minScale = 0.1f;
 
     public bool isPlaced = false;
 
@@ -23,12 +24,15 @@ public class Shape : MonoBehaviour
         {
             // TODO: Figure out if we want GetAxis or GetAxisRaw
             Vector3 moveDir = new Vector3(Input.GetAxis("MoveX"), Input.GetAxis("MoveY"), 0);
-            this.transform.position += Time.deltaTime * moveSpeed * moveDir;
+            gameObject.transform.position += Time.deltaTime * moveSpeed * moveDir;
 
-            this.transform.Rotate(0, 0, Time.deltaTime * rotateSpeed * Input.GetAxis("Rotate"));
+            gameObject.transform.Rotate(0, 0, Time.deltaTime * rotateSpeed * Input.GetAxis("Rotate"));
 
             Vector3 scaleDir = new Vector3(Input.GetAxis("ScaleX"), Input.GetAxis("ScaleY"), 0);
-            this.transform.localScale += Time.deltaTime * moveSpeed * scaleDir;
+            Vector3 newScale = gameObject.transform.localScale + Time.deltaTime * moveSpeed * scaleDir;
+            newScale.x = Mathf.Max(minScale, newScale.x);
+            newScale.y = Mathf.Max(minScale, newScale.y);
+            gameObject.transform.localScale = newScale;
         }
     }
 
