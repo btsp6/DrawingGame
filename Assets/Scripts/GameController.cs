55b052abdd996a3f490b25670fdae2cbfcf89821
ed1d1a8d <tong.t.wang@gmail.com>
2020-02-18 15:27:13 -0500

    Shapes are able to be spawned via button presses

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
+    public GameObject[] shapes;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void SpawnShape(string shapeTag)
+    {
+        foreach (GameObject shape in shapes)
+        {   
+            if (shape.CompareTag(shapeTag))
+            {
+                print("Spawning shape!");
+                Destroy(GameData.activeShape);
+                GameData.activeShape = Instantiate(shape);
+                GameData.activeShape.transform.position = new Vector3(0, 0, -1);
+                break;
+            }
+        }
     }
 }
