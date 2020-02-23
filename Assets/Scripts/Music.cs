bd0e38c5c5a721cd35e13b844f2a0234c9851744
ed1d1a8d <tong.t.wang@gmail.com>
2020-02-23 01:48:49 -0500

    Add background music

---
+++ Assets/Scripts/Music.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music : MonoBehaviour
+{
+    public static Music Instance { get; private set; } = null;
+
+    void Awake()
+    {
+        print("HI!");
+        if (Instance != null && Instance != this)
+        {
+            print("Destroying myself");
+            Destroy(this.gameObject);
+            return;
+        }
+
+        print("Not destroying myself");
+        Instance = this;
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    void Start() {
+    }
+
+    void Update() {
+    }
+}
